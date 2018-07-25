a0154859fb933228fc20d3ecbab7955fc39a713a
Gianfranco Bogetti <gbogetti@dc.uba.ar>
2018-07-25 17:46:58 -0300

    Primera clase

---
+++ UnityGS/Assets/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour {
+
+	int frame = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello Word");
+	}
+
+	int input;
+	void Update ()
+	{
+		if (Input.anyKeyDown)
+		{
+			System.String tempInput = Input.inputString;
+			int.TryParse(tempInput, out input);
+			Debug.Log("Apretaste la letra: " + tempInput);
+		}
+
+		else if (Input.GetKeyDown(KeyCode.F))
+		{
+			Debug.Log("Hola Santi estamos en el frame: " + frame);
+		}
+		frame++;
+	}
+	// Update is called once per frame
+}
