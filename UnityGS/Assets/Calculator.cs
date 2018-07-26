d0ba612f3a088dc87b26ef23ed9a287fc3ae2c89
Gianfranco Bogetti <gbogetti@dc.uba.ar>
2018-07-26 18:17:56 -0300

    clase 2

--- UnityGS/Assets/Calculator.cs
+++ UnityGS/Assets/Calculator.cs
@@ -1,30 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Calculator : MonoBehaviour {
+class Calculator : MonoBehaviour {
 
+	[SerializeField] Text textComponent;
+	System.String globalInput;
+	System.String auxGlobalInput = "0";
+	System.String operand = "INVALID";
 	int frame = 0;
-	// Use this for initialization
-	void Start () {
-		Debug.Log("Hello Word");
+
+	public void Start()
+	{
+		globalInput = "0";
 	}
 
-	int input;
-	void Update ()
+	public void Update()
 	{
-		if (Input.anyKeyDown)
+		if((Input.anyKeyDown) && (Input.inputString != ""))
 		{
-			System.String tempInput = Input.inputString;
-			int.TryParse(tempInput, out input);
-			Debug.Log("Apretaste la letra: " + tempInput);
+			if((Input.GetKeyDown(KeyCode.Delete)) || (Input.GetKeyDown(KeyCode.Escape)))
+			{
+				globalInput = "0";
+			}
+			else if(Input.GetKeyDown(KeyCode.Backspace))
+			{
+				globalInput = globalInput.Remove(globalInput.Length-1, 1);
+				if(globalInput == "")
+				{
+					globalInput = "0";
+				}
+			}
+			else if((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.Return)))
+			{
+				int num1 = 0;
+				int num2 = 0;
+				int result = 0;
+				int.TryParse(auxGlobalInput, out num1);
+				int.TryParse(globalInput, out num2);
+				if(operand == "+"){result = num1 + num2;}
+				else if(operand == "-"){result = num1 - num2;}
+				else if(operand == "*"){result = num1 * num2;}
+				else if(operand == "/"){result = num1 / num2;}
+
+				if(!(operand == "INVALID"))
+				{
+					operand = "INVALID";
+					globalInput = result.ToString();
+					auxGlobalInput = globalInput;
+				}
+			}
+			else if((Input.GetKeyDown(KeyCode.Plus)) || (Input.GetKeyDown(KeyCode.Minus)) ||
+					(Input.GetKeyDown(KeyCode.Asterisk)) || (Input.GetKeyDown(KeyCode.Slash)) )
+			{
+				int num1 = 0;
+				int num2 = 0;
+				int result = 0;
+				int.TryParse(auxGlobalInput, out num1);
+				int.TryParse(globalInput, out num2);
+				if(operand == "+"){result = num1 + num2;}
+				else if(operand == "-"){result = num1 - num2;}
+				else if(operand == "*"){result = num1 * num2;}
+				else if(operand == "/"){result = num1 / num2;}
+
+				if(operand != "INVALID")
+				{
+					globalInput = result.ToString();
+				}
+				auxGlobalInput = globalInput;
+				operand = Input.inputString;
+				// arreglar
+			}
+			else
+			{
+				System.String tempInput = Input.inputString;
+				if(globalInput == "0" || globalInput == "RESULT")
+				{
+					globalInput = "";
+				}
+				globalInput += tempInput;
+			}
+
+			textComponent.text = globalInput;
 		}
 
-		else if (Input.GetKeyDown(KeyCode.F))
+		if(Input.GetKeyDown(KeyCode.F))
 		{
 			Debug.Log("Hola Santi estamos en el frame: " + frame);
 		}
 		frame++;
 	}
-	// Update is called once per frame
+
 }
